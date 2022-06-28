d1ea68e69922c83a5f24dcb4522647076110f361
TTS <90527370+TallerThanShort@users.noreply.github.com>
2022-06-28 08:26:45 +0200

    y is that two there
    and more worryingly, why does it work with it there

--- c/script.js
+++ c/script.js
@@ -3,7 +3,7 @@ window.onload = function(){
     const fragment = new URLSearchParams(window.location.hash.slice(1));
     const [channel, debug] = [fragment.get('user'), fragment.get('dev')];
 
-    if(channel != null){2 //store is a global var = firestore
+    if(channel != null){ //store is a global var = firestore
         store.collection("c").where("channel", "==", channel)
             .get()
             .then((querySnapshot) => {
