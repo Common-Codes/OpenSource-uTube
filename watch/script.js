947054fbb0fe0b23c4888bd3c64c55cb1dd256d1
TTS <90527370+TallerThanShort@users.noreply.github.com>
2022-06-30 16:12:42 +0200

    5% 1px all that difference

--- watch/script.js
+++ watch/script.js
@@ -9,7 +9,7 @@ window.onload = function() {
             .then((querySnapshot) => {
                 querySnapshot.forEach((doc) => {
                     const vdata = doc.data()
-                    maestro.innerHTML = `<video style="width: 100%;" controls><source src="${vdata.src}" type="video/mp4">Your Browser does not support HTML video tags</video><br><p>${vdata.title}</p><div style="width: 100%; height: 49px; font-size: 16px; border: 3px double black; position: relative; background-color: black;"><button style="position: absolute; top: 8px; right: 16px; background-color: crimson; color: white; height: 35px; width: 20%;">FOLLOW</button><img src="${vdata.uploader}" style="height: 24px; width: 24px; border-radius: 50%; position: absolute; top: 10px; left: 5px;"><p style="position: absolute; top: 0; left: 40px; color: white;">${vdata.channel}</p></div>`;
+                    maestro.innerHTML = `<video style="width: 100%;" controls><source src="${vdata.src}" type="video/mp4">Your Browser does not support HTML video tags</video><br><p>${vdata.title}</p><div style="width: 100%; height: 49px; font-size: 16px; border: 3px double black; position: relative; background-color: black;"><button style="position: absolute; top: 8px; right: 16px; background-color: crimson; color: white; height: 35px; width: 25%;">FOLLOW</button><img src="${vdata.uploader}" style="height: 24px; width: 24px; border-radius: 50%; position: absolute; top: 10px; left: 5px;"><p style="position: absolute; top: -1px; left: 40px; color: white;">${vdata.channel}</p></div>`;
         });
     })
     .catch((error) => {
