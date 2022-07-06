a78dbd33f6d5b72a5af2a66f28b7c563317a80d2
TTS <90527370+TallerThanShort@users.noreply.github.com>
2022-07-06 11:40:13 +0200

    peeegs

--- c/script.js
+++ c/script.js
@@ -24,25 +24,7 @@ window.onload = function(){
             .then((querySnapshot) => {
                 querySnapshot.forEach((doc) => {
                     const vdata = doc.data()
-                    const pig = `div id="profile-banner">
-            <img src="https://media.discordapp.net/attachments/858798985807527956/992489274769412277/banner-5.png" style="width: 100%;">
-        </div>
-        <br>
-        <div id="profile" style="position: relative;">
-             <img src="https://ggpht.ga/icons/LAcbj8kQTYn-6YG1IAjI1yFiUsRCxC2HUpqvPzuyM.png" style="height: 46px; width: 46px; position: absolute; top: -46px; left: 6px; border-radius: 50%;">
-             <button onclick="followFunction()" style="position: absolute; top: 18px; right: 16px; background-color: crimson; color: black; height: 35px; width: 25%;">FOLLOW</button>
-             <p id="name" style="position: absolute; top: 6px; left: 5px;">${water.username}</p>
-             <p id="followers" style="position: absolute; top: -21px; right: 15px; color: lightgrey;">${water.followers} followers</p>
-             <p id="counter" style="position: absolute; top: 32px; left: 5px; color: gray;">${water.videos} videos</p>
-        </div>
-        <div id="videos" onclick="location.href='https://common-codes.github.io/OpenSource-uTube/watch/#vid=${vdata.id}'" style="position: relative; top: 95px; width: 100%;">
-            <div class="video-data"><img src="https://http.cat/404" style="height: 90px; width: 150px; position: absolute; left: 5px;">
-                <b class="video-title" style="position: absolute; left: 160px; top: 8px;">${vdata.title}</b>
-            </div>
-            <br>
-            <div style="position: relative; top: 64px;" class="video-data" onclick="location.href='https://common-codes.github.io/OpenSource-uTube/watch/#vid=${vdata.id}'"><img src="https://http.cat/404" style="height: 90px; width: 150px; position: absolute; left: 5px;">
-            <b class="video-title" style="position: absolute; left: 160px; top: 8px;">${vdata.title}</b>
-        </div>`;
+                    const pig = `<div onclick="location.href='https://common-codes.github.io/OpenSource-uTube/watch/#vid=${vdata.id}'" class="video-data" style="position: relative;"><img src="${vdata.vod}" style="height: 90px; width: 150px; position: absolute; left: 5px;"><b class="video-title" style="position: absolute; left: 160px; top: 8px;">${vdata.title}</b></div><br>`;
                     maestroContent.innerHTML += pig;
     }
 }
