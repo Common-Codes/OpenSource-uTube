8c94a8543febac0413cc2441695c2018fcd7bedf
TTS <90527370+TallerThanShort@users.noreply.github.com>
2022-07-09 20:06:07 +0200

    i used channel positionings on watch pages lel

--- watch/script.js
+++ watch/script.js
@@ -9,7 +9,7 @@ window.onload = function() {
             .then((querySnapshot) => {
                 querySnapshot.forEach((doc) => {
                     const vdata = doc.data()
-                    maestro.innerHTML = `<video style="width: 100%;" controls><source src="${vdata.src}" type="video/mp4">Your Browser does not support HTML video tags</video><br><p>${vdata.title}</p><div style="width: 100%; height: 49px; font-size: 16px; position: relative; background-color: black;"><div id="followbtn"><button onclick="followFunction()" style="position: absolute; top: 18px; right: 16px; background-color: crimson; color: white; height: 35px; width: 25%; border: none;">FOLLOW</button></div><img onclick="location.href='https://common-codes.github.io/OpenSource-uTube/c/#user=${vdata.channel}';" src="${vdata.uploader}" style="height: 24px; width: 24px; border-radius: 50%; position: absolute; top: 10px; left: 5px;"><p id="channelname" onclick="location.href='https://common-codes.github.io/OpenSource-uTube/c/#user=${vdata.channel}';" style="position: absolute; top: -1px; left: 40px; color: white;">${vdata.channel}</p></div>`;
+                    maestro.innerHTML = `<video style="width: 100%;" controls><source src="${vdata.src}" type="video/mp4">Your Browser does not support HTML video tags</video><br><p>${vdata.title}</p><div style="width: 100%; height: 49px; font-size: 16px; position: relative; background-color: gray;"><div id="followbtn"><button onclick="followFunction()" style="position: absolute; top: 8px; right: 16px; background-color: crimson; color: white; height: 35px; width: 25%; border: none;">FOLLOW</button></div><img onclick="location.href='https://common-codes.github.io/OpenSource-uTube/c/#user=${vdata.channel}';" src="${vdata.uploader}" style="height: 24px; width: 24px; border-radius: 50%; position: absolute; top: 10px; left: 5px;"><p id="channelname" onclick="location.href='https://common-codes.github.io/OpenSource-uTube/c/#user=${vdata.channel}';" style="position: absolute; top: -1px; left: 40px; color: white;">${vdata.channel}</p></div>`;
         });
     })
     .catch((error) => {
@@ -24,7 +24,7 @@ const followFunction = () => {
     const user = firebase.auth().currentUser;
     if(user) {
         // TO-DO: API shid so following is a thing.
-        flowbtn.innerHTML = `<button onclick="followFunction()" style="position: absolute; top: 18px; right: 16px; background-color: lightgray; color: black; height: 35px; width: 25%; border: none;">FOLLOWING</button>`;
+        flowbtn.innerHTML = `<button onclick="followFunction()" style="position: absolute; top: 8px; right: 16px; background-color: lightgray; color: black; height: 35px; width: 25%; border: none;">FOLLOWING</button>`;
     } else {
         location.href="https://common-codes.github.io/OpenSource-uTube/login.html";
     }
