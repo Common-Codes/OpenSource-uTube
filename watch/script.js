cad7c4cad2961ee1f75167be897e398a59987338
TTS <90527370+TallerThanShort@users.noreply.github.com>
2022-07-09 20:01:06 +0200

    e
    button div now yay

--- watch/script.js
+++ watch/script.js
@@ -9,7 +9,7 @@ window.onload = function() {
             .then((querySnapshot) => {
                 querySnapshot.forEach((doc) => {
                     const vdata = doc.data()
-                    maestro.innerHTML = `<video style="width: 100%;" controls><source src="${vdata.src}" type="video/mp4">Your Browser does not support HTML video tags</video><br><p>${vdata.title}</p><div style="width: 100%; height: 49px; font-size: 16px; position: relative; background-color: black;"><button id="flowbtn" onclick="followFunction()" style="position: absolute; top: 8px; right: 16px; background-color: crimson; color: white; height: 35px; width: 25%;">FOLLOW</button><img onclick="location.href='https://common-codes.github.io/OpenSource-uTube/c/#user=${vdata.channel}';" src="${vdata.uploader}" style="height: 24px; width: 24px; border-radius: 50%; position: absolute; top: 10px; left: 5px;"><p id="channelname" onclick="location.href='https://common-codes.github.io/OpenSource-uTube/c/#user=${vdata.channel}';" style="position: absolute; top: -1px; left: 40px; color: white;">${vdata.channel}</p></div>`;
+                    maestro.innerHTML = `<video style="width: 100%;" controls><source src="${vdata.src}" type="video/mp4">Your Browser does not support HTML video tags</video><br><p>${vdata.title}</p><div style="width: 100%; height: 49px; font-size: 16px; position: relative; background-color: black;"><div id="followbtn"><button onclick="followFunction()" style="position: absolute; top: 18px; right: 16px; background-color: crimson; color: white; height: 35px; width: 25%; border: none;">FOLLOW</button></div><img onclick="location.href='https://common-codes.github.io/OpenSource-uTube/c/#user=${vdata.channel}';" src="${vdata.uploader}" style="height: 24px; width: 24px; border-radius: 50%; position: absolute; top: 10px; left: 5px;"><p id="channelname" onclick="location.href='https://common-codes.github.io/OpenSource-uTube/c/#user=${vdata.channel}';" style="position: absolute; top: -1px; left: 40px; color: white;">${vdata.channel}</p></div>`;
         });
     })
     .catch((error) => {
@@ -20,12 +20,11 @@ window.onload = function() {
 
 const followFunction = () => {
     const toflow = document.getElementById("channelname").innerText;
-    const flowbtn = document.getElementById("flowbtn");
+    const flowbtn = document.getElementById("followbtn");
     const user = firebase.auth().currentUser;
     if(user) {
-        flowbtn.value = "FOLLOWING"; //for now this just renders a front-end 'following' text on the follow button.
-        flowbtn.color = 'black';
-        flowbtn.backgroundColor = 'lightgray'; //and change its colors. Doesnt update in the API tho.
+        // TO-DO: API shid so following is a thing.
+        flowbtn.innerHTML = `<button onclick="followFunction()" style="position: absolute; top: 18px; right: 16px; background-color: lightgray; color: black; height: 35px; width: 25%; border: none;">FOLLOWING</button>`;
     } else {
         location.href="https://common-codes.github.io/OpenSource-uTube/login.html";
     }
