2261247b48d5d6fde020201ceee117031b8c3364
TTS <90527370+TallerThanShort@users.noreply.github.com>
2022-07-17 11:37:53 +0200

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- c/script.js
+++ c/script.js
@@ -9,7 +9,7 @@ window.onload = function(){
     if(user) {
         store.collection('c').doc(user.uid).get().then(doc => {
             loginD.innerHTML += `<img style="width: 21px; height: 21px; border-radius: 50%; position: absolute; top: -21px; right: -188px;" onclick="location.href='https://common-codes.github.io/uTube/c/@me';" src="${doc.data().img}">`;
-        }
+        })
     } else {
         loginD.innerHTML += `<button style="position: absolute; top: -21px; right: -188px; color: white; background-color: crimson;" onclick="location.href='https://common-codes.github.io/uTube/login.html';">LOGIN</button>`;
         console.log("no auth");
