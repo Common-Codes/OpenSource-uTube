c6ab5f6da6a275df9df30f30eb13eae0e323b543
TTS <90527370+TallerThanShort@users.noreply.github.com>
2022-09-10 18:11:04 +0200

    fix player width and height issues

--- watch/script.js
+++ watch/script.js
@@ -2,6 +2,7 @@ window.onload = function() {
     const maestro = document.getElementById("master-div");
     const fragment = new URLSearchParams(window.location.hash.slice(1));
     const [video, debug] = [fragment.get('vid'), fragment.get('dev')];
+    const playerWidth = window.innerWidth;
 
     if(video != null){
         store.collection("vid").where("id", "==", video)
@@ -9,7 +10,11 @@ window.onload = function() {
             .then((querySnapshot) => {
                 querySnapshot.forEach((doc) => {
                     const vdata = doc.data()
-                    maestro.innerHTML = `<video style="width: 100%;" controls loop poster="${vdata.vod}"><source src="${vdata.src}" type="video/mp4">Your Browser does not support HTML video tags</video><br><p>${vdata.title}</p><div style="width: 100%; height: 49px; font-size: 16px; position: relative; background-color: gray;"><div id="followbtn"><button onclick="followFunction()" style="position: absolute; top: 8px; right: 16px; background-color: crimson; color: white; height: 35px; width: 25%; border: none;">FOLLOW</button></div><img onclick="location.href='https://common-codes.github.io/uTube/c/#user=${vdata.channel}';" src="${vdata.uploader}" style="height: 24px; width: 24px; border-radius: 50%; position: absolute; top: 10px; left: 5px;"><p id="channelname" onclick="location.href='https://common-codes.github.io/uTube/c/#user=${vdata.channel}';" style="position: absolute; top: -1px; left: 40px; color: white;">${vdata.channel}</p></div>`;
+                    if(playerWidth > '800'){
+                        maestro.innerHTML = `<video style="width: 60%;" controls loop poster="${vdata.vod}"><source src="${vdata.src}" type="video/mp4">Your Browser does not support HTML video tags</video><br><p>${vdata.title}</p><div style="width: 60%; height: 49px; font-size: 16px; position: relative; background-color: gray;"><div id="followbtn"><button onclick="followFunction()" style="position: absolute; top: 8px; right: 16px; background-color: crimson; color: white; height: 35px; width: 25%; border: none;">FOLLOW</button></div><img onclick="location.href='https://common-codes.github.io/uTube/c/#user=${vdata.channel}';" src="${vdata.uploader}" style="height: 24px; width: 24px; border-radius: 50%; position: absolute; top: 10px; left: 5px;"><p id="channelname" onclick="location.href='https://common-codes.github.io/uTube/c/#user=${vdata.channel}';" style="position: absolute; top: -1px; left: 40px; color: white;">${vdata.channel}</p></div>`;
+                    } else{
+                        maestro.innerHTML = `<video style="width: 100%;" controls loop poster="${vdata.vod}"><source src="${vdata.src}" type="video/mp4">Your Browser does not support HTML video tags</video><br><p>${vdata.title}</p><div style="width: 100%; height: 49px; font-size: 16px; position: relative; background-color: gray;"><div id="followbtn"><button onclick="followFunction()" style="position: absolute; top: 8px; right: 16px; background-color: crimson; color: white; height: 35px; width: 25%; border: none;">FOLLOW</button></div><img onclick="location.href='https://common-codes.github.io/uTube/c/#user=${vdata.channel}';" src="${vdata.uploader}" style="height: 24px; width: 24px; border-radius: 50%; position: absolute; top: 10px; left: 5px;"><p id="channelname" onclick="location.href='https://common-codes.github.io/uTube/c/#user=${vdata.channel}';" style="position: absolute; top: -1px; left: 40px; color: white;">${vdata.channel}</p></div>`;
+                    }
         });
     })
     .catch((error) => {
